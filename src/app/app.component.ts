eecb9aabbc9a3ec9ff2503c521c588653045d096
Noah Bemisderfer <nbemisderfer@yahoo.com>
2021-01-15 21:23:25 -0600

    Commiting entire project

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mens-website';
+  num1: number;
+  num2: number;
+  result: number;
+
+  add(){
+    this.result = this.num1 + this.num2;
+  }
+
+  subtract(){
+    this.result = this.num1 - this.num2;
+  }
+
+  multiply(){
+    this.result = this.num1 * this.num2;
+  }
+
+  divide(){
+    this.result = this.num1 / this.num2;
+  }
+}
